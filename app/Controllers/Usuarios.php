f8c28097c7c7d755529106ca7c5f642ebfcba2af
david rojas <josedavidrojas323@gmail.com>
2025-06-25 04:53:49 -0500

    Auto commit

--- app/Controllers/Usuarios.php
+++ app/Controllers/Usuarios.php
@@ -68,7 +68,7 @@ public function eliminar($id)
 
     } catch (\Exception $e) {
         //return redirect()->back()->with('error', 'No se puede eliminar el usuario porque tiene relaciones en otras tablas.');//
-        return redirect()->back()->with('error', 'No se puede eliminar el usuario porque tiene relaciones en otras tablas.');
+        return redirect()->to(base_url('/usuarios'))->with('error', 'No se puede eliminar el usuario porque tiene relaciones en otras tablas.');
 
     }
 }
