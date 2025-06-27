98feacfad9acd4473b84fb5abe1da8a8c227d36e
david rojas <josedavidrojas323@gmail.com>
2025-06-27 03:29:01 -0500

    Auto commit

--- app/Views/usuarios/index.php
+++ app/Views/usuarios/index.php
@@ -410,7 +410,7 @@
                 <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
                     <i class="bi bi-x-circle me-1"></i>Cancelar
                 </button>
-                <a id="confirmDeleteBtn" href="<?= base_url('usuarios/eliminar/' . $usuario['idUsuario']) ?>" class="btn btn-danger">
+                <a id="confirmDeleteBtn" href="#" class="btn btn-danger">
                     <i class="bi bi-trash me-1"></i>Eliminar Usuario
                 </a>
             </div>
