7c3957de66100bdf6dd10d1ea80840b002d03299
david rojas <josedavidrojas323@gmail.com>
2025-06-27 03:39:07 -0500

    Auto commit

--- app/Views/plantilla/layout_usuario.php
+++ app/Views/plantilla/layout_usuario.php
@@ -6,11 +6,12 @@
 <!-- Asegúrate de tener estas líneas en tu <head> -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-<!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
 
 </head>
 <body>
     <?php echo $this->renderSection('contenido'); ?>
 </body>
+    <!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </html>
