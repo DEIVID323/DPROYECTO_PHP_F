85b170046bdb10fda9672c51485915e8bc139411
david rojas <josedavidrojas323@gmail.com>
2025-06-27 05:51:11 -0500

    Auto commit

--- app/Views/plantilla/layout_usuario.php
+++ app/Views/plantilla/layout_usuario.php
@@ -4,18 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= base_url('css/usuarios.css') ?>">
-    <script>
-    const BASE_URL = '<?= base_url() ?>';
-    </script>
-    <script src="<?= base_url('js/usuarios.js') ?>"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
 
-    
-    <!-- Bootstrap JS (solo una vez y al final) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="<?= base_url('css/usuarios.css') ?>">
 
 
 
@@ -23,4 +15,12 @@
 <body>
     <?php echo $this->renderSection('contenido'); ?>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('js/usuarios.js') ?>"></script>
+    <script>
+    const BASE_URL = '<?= base_url() ?>';
+    </script>
+
+
+
 </html>
