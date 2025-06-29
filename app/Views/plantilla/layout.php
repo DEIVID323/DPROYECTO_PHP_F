e9b6684b9f2808bdf2e5a105da49bf0325f8d4dd
david rojas <josedavidrojas323@gmail.com>
2025-06-29 15:36:54 -0500

    Auto commit

--- app/Views/plantilla/layout.php
+++ app/Views/plantilla/layout.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DMC VISVARD - Distribuidora de Productos Ópticos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo base_url ('css/main.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/main.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('css/style.css'); ?>">
-    <title>WEB</title>
+   <title>DMC VISVARD - Distribuidora de Productos Ópticos</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="<?php   base_url('js/main.js'); ?>" defer></script>
+    <script src="<?php echo base_url('js/main.js'); ?>" defer></script>
 </head>
 <body>
     <?php echo $this->renderSection('contenido'); ?>
