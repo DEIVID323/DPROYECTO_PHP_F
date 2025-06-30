d0a7cfd2aae558940f31c96771dd802a19806e49
david rojas <josedavidrojas323@gmail.com>
2025-06-30 02:12:55 -0500

    Auto commit

---
+++ app/Views/catalogo productos/index.php
@@ -0,0 +1,42 @@
+<?php  echo $this->extend('plantilla/layout_catalogo'); ?>
+<?php echo $this->section('contenido'); ?>
+    <body>
+        <section class="slider-main">
+            <div class="container">
+                <div class="logo">
+                    <H1>DMC VISVARD</H1>
+                </div>
+                <div class="slider-content-wrap">                
+                    <div class="slider-content">
+                        <h2 class="heading-style-2">Accesorios Para Gafas</h2>
+                        <p>La Mejor Calidad</p>
+                        <h3 class="heading-style-2">$30.000</h3>
+                        <div class="social-icons">
+                            <a href="https://www.instagram.com/jdrfilmmaker?igsh=NjVqdWEzdnR1bTAx"><img src="https://www.yudiz.com/codepen/headphone-slider/instagram-icon.svg" alt="instagram"></a>
+                            <a href="https://www.facebook.com/share/1Vep3svjjQ/"><img src="https://www.yudiz.com/codepen/headphone-slider/facbook-icon.svg" alt="facebook"></a>
+                            <a href="https://x.com/jdrfilmms"><img src="https://www.yudiz.com/codepen/headphone-slider/twiter-icon.svg" alt="twitter"></a>
+                        </div>
+                    </div>               
+                </div>         
+            </div>
+            <div class="slider-images">       
+                <img class="slider-image" src="imagenes/Líquido Verde .png" alt="headphone image">        
+                <img class="slider-image" src="imagenes/C azul_.png" alt="headphone image">        
+                <img class="slider-image" src="imagenes/Montura sol.png" alt="headphone image">        
+                <img class="slider-image" src="imagenes/Estuche Rojo C.png" alt="headphone image">        
+                <img class="slider-image" src="imagenes/Estuche F Negro_.png" alt="headphone image">
+                <img class="slider-image" src="imagenes/C Rojo_.png" alt="headphone image">
+                <img class="slider-image" src="imagenes/Estuche Cafe C.png" alt="headphone image">           
+            </div> 
+            <div id="backgrounds">
+                <div class="background" style="background: radial-gradient(50% 50% at 50% 50%, #C7F6D0 0%, #7CB686 92.19%);"></div>
+                <div class="background" style="background: radial-gradient(50% 50% at 50% 50%, #D1E4F6 0%, #5F9CCF 100%);"></div>
+                <div class="background" style="background: radial-gradient(50% 50% at 50% 50%, #f5b9b5 0%, #ec8680 100%);"></div>
+                <div class="background" style="background: radial-gradient(50% 50% at 50% 50%, #ff3e3e 0%, #ef1e1e 100%);"></div>
+                <div class="background" style="background: radial-gradient(50% 50% at 50% 50%, #6B6B6B 0%, #292929 100%);"></div>
+                <div class="background" style="background: radial-gradient(50% 50% at 50% 50%, #f35959c3 0%, #fa5d5dc2 100%);"></div>
+                <div class="background" style="background: radial-gradient(50% 50% at 50% 50%, #6f3c1c 0%, #713a19 100%);"></div>
+            </div>
+        </section>   
+    </body>
+<?php echo $this->endSection(); ?>
