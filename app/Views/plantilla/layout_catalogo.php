105bb7f618d43280380a10d4dbed24927af1a0e2
david rojas <josedavidrojas323@gmail.com>
2025-07-01 02:03:30 -0500

    Auto commit

--- app/Views/plantilla/layout_catalogo.php
+++ app/Views/plantilla/layout_catalogo.php
@@ -3,9 +3,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#1bb566">
+    <title>DMC Visvard - Catálogo Óptico</title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;900&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/catalogo.css">
    
-    <title>Catalogo</title>
 </head>
 <body>
     <?php echo $this->renderSection('contenido'); ?>
