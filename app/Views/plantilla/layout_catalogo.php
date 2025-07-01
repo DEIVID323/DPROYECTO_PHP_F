77c94b5260fb78604e16da28077850fb41285fef
david rojas <josedavidrojas323@gmail.com>
2025-07-01 00:32:56 -0500

    Auto commit

--- app/Views/plantilla/layout_catalogo.php
+++ app/Views/plantilla/layout_catalogo.php
@@ -3,12 +3,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#1bb566">
-    <link rel="stylesheet" href="css/productos.css">
+    <link rel="stylesheet" href="css/catalogo.css">
    
     <title>Productos</title>
 </head>
 <body>
     <?php echo $this->renderSection('contenido'); ?>
 </body>
- <script src="js/productos.js"defer></script>
+ <script src="js/catalogo.js"defer></script>
 </html>
