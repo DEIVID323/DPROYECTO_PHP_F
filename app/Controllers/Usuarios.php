806b5afe92c01b014c69df1480f1d0a1577a5201
david rojas <josedavidrojas323@gmail.com>
2025-07-08 21:17:47 -0500

    Auto commit

--- app/Controllers/Usuarios.php
+++ app/Controllers/Usuarios.php
@@ -1,7 +1,8 @@
 <?php
 namespace App\Controllers;
 use App\Models\UsuarioModel;
-
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class Usuarios extends BaseController  
 {
